a56520f17a371695376c9a5d49a32afd2d03b067
gjchow <gjchow@users.noreply.github.com>
2020-09-03 11:13:43 -0400

    Created the starter for the html and js

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return 'Hello World!'
-
-
-@app.route('/<name>')
-def hello_name(name):
-    return f'Hello {name}!'
+def home():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
