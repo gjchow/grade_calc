c7ef2c62403c7093340e0434ce845ba1f0346ec0
gjchow <gjchow@users.noreply.github.com>
2020-09-10 18:20:21 -0400

    Added help instructions and centred the elements

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -4,14 +4,14 @@ $(document).ready(function() {
     let temp = "";
     $("button").click(function() {
         if ($(this).attr("class") == "add"){
-//            alert("Clicked add");
-            num += 1;
+            num += 1
             len += 1
             temp = '<div class="in'+num+'"><p><input type="text" id="mark'+num+'" name="mark'+num+'" placeholder="Mark"><input type="text" id="percent'+num+'" name="percent'+num+'" placeholder="Percentage of grade"><br></p></div>';
             $(".main").append(temp);
         }
         else if($(this).attr("class") == "remove"){
-            if (num == 1){
+            alert("remove")
+            if (len == 1){
                 alert("Must have at least 1 entry")
             }
             else {
